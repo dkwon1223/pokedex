6626b42190ad73a0e418eaa0e099b88ce47bf12d
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-05-23 12:28:59 -0600

    Complete basic hero page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,12 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-to-b-grey': 'linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0))'
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
